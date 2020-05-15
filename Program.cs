1a353cfc823d2696432ba9566afd4c1a7e215e10
connor <vbrookesy@hotmail.co.uk>
2020-05-15 14:14:13 +0100

    Wrapped my new code inside a new function to split the code up into chunks

--- Program.cs
+++ Program.cs
@@ -6,26 +6,22 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            int[,] numberGrid =
-            {
-                {1, 2 },
-                {3, 4 },
-                {5, 6 }
-            };
-            int[,] myArray = new int[2, 3];
-            Console.WriteLine(numberGrid[0, 0]);
-
-            Console.ReadLine();
-
-            Console.Write("Enter a number: ");
-            int num1 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter a second number: ");
-            int num2 = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine(num1 / num2);
-
-            Console.ReadLine();
 
         }
-    }
+
+            static void throwErrorFunction()
+            {
+                Console.Write("Enter a number: ");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter a second number: ");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine(num1 / num2);
+
+                Console.ReadLine();
+            }
+            
+
+     }
 }
+
