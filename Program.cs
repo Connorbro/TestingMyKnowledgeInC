e915328b13b91a51410b7f10c47338fb7c55c3d6
connor <vbrookesy@hotmail.co.uk>
2020-05-15 14:01:53 +0100

    Setting up a 2D array

--- Program.cs
+++ Program.cs
@@ -6,7 +6,10 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[,] numberGrid =
+            {
+
+            }
         }
     }
 }
